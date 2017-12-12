cf876665bb34a7b3b864f103a6724d2b6812c65e
Felix Chen <felix.chen@sap.com>
2017-12-12 16:52:39 +0800

    init

---
+++ Gemfile
@@ -0,0 +1,6 @@
+gem 'os', '~> 1.0'
+gem 'selenium-webdriver'
+gem 'pry', '0.10.1'
+gem 'pry-rails', '0.3.2'
+gem 'pry-nav'
+gem 'pry-stack_explorer'
---
+++ index.rb
@@ -0,0 +1,26 @@
+require 'selenium-webdriver'
+require 'pry'
+require 'os'
+
+# binding.pry
+base = Dir.pwd + '/chrome-driver'
+if OS.mac?
+  ENV['PATH'] += "#{File::PATH_SEPARATOR}#{base}/mac"
+elsif OS.linux?
+  ENV['PATH'] += "#{File::PATH_SEPARATOR}#{base}/linux"
+elsif OS.windows?
+  ENV['PATH'] += "#{File::PATH_SEPARATOR}#{base}/linux"
+end
+
+binding.pry
+
+options = Selenium::WebDriver::Chrome::Options.new()
+# options = Selenium::WebDriver::Chrome::Options.new(args: ['headless'])
+
+driver = Selenium::WebDriver.for(:chrome, options: options)
+
+driver.get('http://stackoverflow.com/')
+
+puts driver.title
+
+driver.quit
