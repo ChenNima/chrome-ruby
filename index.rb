cc8df0033f4297cd82d6e77cea9ea3520665e365
Felix Chen <felix.chen@sap.com>
2017-12-13 17:37:45 +0800

    add macos path

--- index.rb
+++ index.rb
@@ -6,13 +6,13 @@ require 'os'
 base = Dir.pwd + '/chrome-driver'
 if OS.mac?
   ENV['PATH'] += "#{File::PATH_SEPARATOR}#{base}/mac"
+  Selenium::WebDriver::Chrome.path = "#{Dir.pwd}/chrome/Chromium.app/Contents/MacOS/Chromium"
 elsif OS.linux?
   ENV['PATH'] += "#{File::PATH_SEPARATOR}#{base}/linux"
+  Selenium::WebDriver::Chrome.path = "#{Dir.pwd}/chrome/chrome"
 elsif OS.windows?
   ENV['PATH'] += "#{File::PATH_SEPARATOR}#{base}/windows"
 end
-
-Selenium::WebDriver::Chrome.path = "#{Dir.pwd}/chrome/chrome" if OS.linux?
 
 # options = Selenium::WebDriver::Chrome::Options.new()
 options = Selenium::WebDriver::Chrome::Options.new(args: ['headless'])
