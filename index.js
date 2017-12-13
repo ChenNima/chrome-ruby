98b17ddbb2dce9e539a1075f34af2cfdde471c88
Felix Chen <felix.chen@sap.com>
2017-12-13 15:50:32 +0800

    remove try

--- index.js
+++ index.js
@@ -3,11 +3,7 @@ console.log('import puppeteer');
 
 (async () => {
   console.log('start chrome');
-  try {
-    const browser = await puppeteer.launch();
-  } catch (error) {
-    console.log(error);
-  }
+  const browser = await puppeteer.launch();
   console.log('start getting page');
   const page = await browser.newPage();
   await page.goto('http://stackoverflow.com/');
