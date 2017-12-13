cc24859035406ff55a30c950ce4608f0cee6f5d0
Felix Chen <felix.chen@sap.com>
2017-12-13 14:34:41 +0800

    add try catch

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ console.log('import puppeteer');
 
 (async () => {
   console.log('start chrome');
-  const browser = await puppeteer.launch();
+  try {
+    const browser = await puppeteer.launch();
+  } catch (error) {
+    console.log(error);
+  }
   console.log('start getting page');
   const page = await browser.newPage();
   await page.goto('http://stackoverflow.com/');
